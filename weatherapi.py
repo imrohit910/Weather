c09061927fe131cffff4d9b03f897b9c98fb7011
Rohit Gupta <75831028+imrohit910@users.noreply.github.com>
2023-11-10 00:11:52 +0530

    Update weatherapi.py

--- weatherapi.py
+++ weatherapi.py
@@ -6,7 +6,7 @@ def	weather(query):
 	querystring = {"city": "Seattle"}
 
 	headers = {
-		"X-RapidAPI-Key": "16f6e00746msh9d310980b0cde54p157b7fjsn1385f9e4a29d",
+		"X-RapidAPI-Key": "################",
 		"X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com"
 	}
 
